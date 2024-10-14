4e3f7fdd12417f88d085b7de6cf327e9a1e4cbbd
Leonid Tsypkin <caudate7855@gmail.com>
2024-10-14 21:56:55 +0700

    - feat: slot moving base logic created

--- Assets/Project/Scripts/UI/Overlays/Inventory/InventorySlot.cs
+++ Assets/Project/Scripts/UI/Overlays/Inventory/InventorySlot.cs
@@ -4,6 +4,13 @@ namespace Project.Scripts.Overlays.Inventory
 {
     public class InventorySlot : MonoBehaviour
     {
-        
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private Vector2 _position;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+            _position = _rectTransform.position;
+        }
     }
 }
--- Assets/Project/Scripts/UI/Overlays/Inventory/Item/InventoryItem.cs
+++ Assets/Project/Scripts/UI/Overlays/Inventory/Item/InventoryItem.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Project.Scripts.Overlays.Inventory
 {
-    public class InventoryItem : MonoBehaviour
+    public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
         [SerializeField] private Image _background;
         [SerializeField] private Image _itemImage;
+
+        [SerializeField] private InventorySlot _inventorySlot;
         
         private void Start()
         {
@@ -21,6 +25,37 @@ namespace Project.Scripts.Overlays.Inventory
         private void SetItemImage()
         {
             
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            transform.position = Input.mousePosition;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = Input.mousePosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            PutItemInSlot();
+        }
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponent<InventorySlot>() != null)
+            {
+                _inventorySlot = other.GetComponent<InventorySlot>();
+            }
+        }
+
+        private void PutItemInSlot()
+        {
+            if (_inventorySlot != null)
+            {
+                transform.position = _inventorySlot.transform.position;
+            }
         }
     }
 }
