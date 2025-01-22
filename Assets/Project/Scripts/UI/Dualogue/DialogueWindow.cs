410bf6d51bd8d0081347b06544f74601efa3cef2
Leonid Tsypkin <caudate7855@gmail.com>
2025-01-22 19:55:36 +0700

    - feat: dialogue system updated

--- Assets/Project/Scripts/UI/Dualogue/DialogueWindow.cs
+++ Assets/Project/Scripts/UI/Dualogue/DialogueWindow.cs
@@ -1,9 +1,21 @@
 using Itibsoft.PanelManager;
+using TMPro;
+using UnityEngine;
 
 namespace Project.Scripts
 {
     public class DialogueWindow : PanelBase
     {
+        public TMP_Text Text => _text;
+        public GameObject TextWindow => _textWindow;
+        public Vector2 StartPosition => _startPosition;
+        public Vector2 EndPosition => _endPosition;
         
+        
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private GameObject _textWindow;
+
+        [SerializeField] private Vector2 _startPosition;
+        [SerializeField] private Vector2 _endPosition;
     }
 }
--- Assets/Project/Scripts/UI/Dualogue/DialogueWindowController.cs
+++ Assets/Project/Scripts/UI/Dualogue/DialogueWindowController.cs
@@ -1,5 +1,7 @@
+using DG.Tweening;
 using Itibsoft.PanelManager;
 using Project.Scripts.Core;
+using TMPro;
 using UnityEngine;
 using Zenject;
 
@@ -11,17 +13,41 @@ namespace Project.Scripts
         public NpcTypes CurrentNpcType;
         
         [Inject] private DialogueSystemManager _dialogueSystemManager;
+
+        private TMP_Text _text;
+        private GameObject _textWindow;
+
+        private RectTransform _rectTransform;
+        private Vector2 _startPosition;
+        private Vector2 _endPosition;
+        
+        private float _moveDuration = 0.8f;
+        private float _fadeDuration = 0.8f;
         
         protected override void Initialize()
         {
-            
+            _text = Panel.Text;
+            _textWindow = Panel.TextWindow;
+            _startPosition = Panel.StartPosition;
+            _endPosition = Panel.EndPosition;
+
+            _rectTransform = _textWindow.GetComponent<RectTransform>();
         }
 
         public void ShowDialogueText()
         {
-            var text = _dialogueSystemManager.GetCurrentDialogue(CurrentNpcType);
-            
-            Debug.Log(text);   
+             _text.text = _dialogueSystemManager.GetCurrentDialogue(CurrentNpcType);
+             
+             _rectTransform.anchoredPosition = _startPosition;
+             
+             var color = _text.color;
+             color.a = 0;
+             _text.color = color;
+             
+             Sequence sequence = DOTween.Sequence();
+             sequence.
+                 Join(_rectTransform.DOAnchorPos(_endPosition, _moveDuration).SetEase(Ease.Linear))
+                 .Join(_text.DOFade(1f, _fadeDuration));
         }
 
         public void ShowSpeakerImage()
