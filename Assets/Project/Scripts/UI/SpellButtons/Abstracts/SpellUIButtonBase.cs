658110b230e8e3693c0442e731a93a9ad381dff7
Leonid Tsypkin <caudate7855@gmail.com>
2025-02-02 18:49:36 +0700

    - feat: spell ui drag base logic created

--- Assets/Project/Scripts/UI/SpellButtons/Abstracts/SpellUIButtonBase.cs
+++ Assets/Project/Scripts/UI/SpellButtons/Abstracts/SpellUIButtonBase.cs
@@ -7,9 +7,9 @@ namespace Project.Scripts
 {
     [RequireComponent(typeof(Image))]
     [RequireComponent(typeof(Button))]
-    public class SpellUIButtonBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class SpellUIButtonBase : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
     {
-        [SerializeField] private Sprite _defaultSprite;
+        [SerializeField] private Sprite _emptySpellSprite;
         
         private Image _image;
         private Button _button;
@@ -19,7 +19,10 @@ namespace Project.Scripts
         private SpellTipHandler _spellTipHandler;
         
         private bool _isSetted;
-
+        
+        private Vector2 _savedPosition;
+        private bool _isDragging;
+        
         private void Awake()
         {
             _button = GetComponent<Button>();
@@ -45,7 +48,7 @@ namespace Project.Scripts
         public void ClearSpell()
         {
             _isSetted = false;
-            _image.sprite = _defaultSprite;
+            _image.sprite = _emptySpellSprite;
         }
 
         public void SetSpellTipHandler(SpellTipHandler spellTipHandler)
@@ -56,7 +59,7 @@ namespace Project.Scripts
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            if (!_isSetted)
+            if (!_isSetted || _isDragging)
             {
                 return;
             }
@@ -78,6 +81,26 @@ namespace Project.Scripts
         {
             _spellTip.SetSpell(_spell);
             _spellTip.Open();
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _savedPosition = transform.position;
+            transform.position = Input.mousePosition;
+            
+            _isDragging = true;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = Input.mousePosition; 
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            transform.position = _savedPosition;
+            
+            _isDragging = false;
         }
     }
 }
