9a98b618679768bdbb742ab522bd119b7d42dff4
Leonid Tsypkin <cudate7855@gmail.com>
2025-03-19 21:33:06 +0700

    - feat: base load logic for sandbox created

--- Assets/Project/Scripts/Boot/SandBoxBoot.cs
+++ Assets/Project/Scripts/Boot/SandBoxBoot.cs
@@ -1,6 +1,4 @@
-﻿using Cysharp.Threading.Tasks;
-using Itibsoft.PanelManager;
-using Project.Scripts.Core;
+﻿using Project.Scripts.Core;
 using UnityEngine;
 using Zenject;
 
@@ -8,39 +6,22 @@ namespace Project.Scripts
 {
     public class SandBoxBoot : LevelBootBase
     {
-        [Inject] private IPanelManager _panelManager;
+        [SerializeField] private SandBoxController _sandBoxController;
         
-        [Inject] private SandBoxFactory _sandBoxFactory;
-        [Inject] private NpcFactory _npcFactory;
         [Inject] private CorpseFactory _corpseFactory;
-        [Inject] private InteractableFactory _interactableFactory;
-        [Inject] private DialogueSystemManager _dialogueSystemManager;
         [Inject] private EnemyFactory _enemyFactory;
 
-        private SandBoxController _sandBoxController;
-        
         protected override async void Initialize()
         {
-            _dialogueSystemManager.Initialize();
-            
-            _sandBoxController =  await _sandBoxFactory.Create<SandBoxController>();
-
-            _interactableFactory.CreateBook(_sandBoxController.GetBookParentObject());
-            _interactableFactory.CreateExit(_sandBoxController.GetExitParentObject());
-            
-            await _npcFactory.CreateNpcAsync<Undertaker>(NpcTypes.Undertaker, _sandBoxController.GetNpcSpawnPosition(NpcTypes.Undertaker));
-            await _npcFactory.CreateNpcAsync<Blacksmith>(NpcTypes.Blacksmith, _sandBoxController.GetNpcSpawnPosition(NpcTypes.Blacksmith));
-            await _npcFactory.CreateNpcAsync<Trader>(NpcTypes.Trader, _sandBoxController.GetNpcSpawnPosition(NpcTypes.Trader));
             await _corpseFactory.CreateCustomCorpse(true,false,false,true, new Vector3(3,0,2));
             await _corpseFactory.CreateDefaultCorpse(new Vector3(3,0,0));
 
-            await CreateTestEnemies();
+            CreateTestEnemies();
         }
-        
-        private async UniTask CreateTestEnemies()
+
+        private async void CreateTestEnemies()
         {
-            await _enemyFactory.Create<EnemyMelee>(EnemyTypes.Melee, new Vector3(1, 0, 0));
-            await _enemyFactory.Create<EnemyRange>(EnemyTypes.Range, new Vector3(2, 0, 0));
+            await _enemyFactory.Create<EnemyRange>(EnemyTypes.Range, new Vector3(5,0,0));
         }
     }
 }
