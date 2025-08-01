8a5bf6b56f158d4f9c80023881ef605f1ee05f19
Leonid Tsypkin <caudate7855@gmail.com>
2025-08-02 06:09:51 +0700

    - feat: boot auto load editor logic created

---
+++ Assets/Editor/AutoBootLoader/AutoBootLoader.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Editor.AutoBootLoader
+{
+    [InitializeOnLoad]
+    public static class AutoBootLoader
+    {
+        private const string BootScenePath = "Assets/Project/Scenes/Boot/BootScene.unity";
+
+        static AutoBootLoader()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeChanged;
+        }
+        
+        private static void OnPlayModeChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingEditMode) 
+                return;
+            
+            if (SceneManager.GetActiveScene().path.Equals(BootScenePath)) 
+                return;
+            
+            EditorApplication.isPlaying = false;
+
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+            EditorSceneManager.OpenScene(BootScenePath);
+
+            EditorApplication.delayCall += () => { EditorApplication.isPlaying = true; };
+        }
+    }
+}
