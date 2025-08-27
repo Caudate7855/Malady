c15ad4efb9c1a2d9948945e97179cbc790c2ae1d
Leonid Tsypkin <caudate7855@gmail.com>
2025-08-27 13:31:39 +0700

    - feat: skill tree page change logic created

--- Assets/Project/Scripts/UI/Overlays/SkillTree/Tree/PassiveSkillTreeView.cs
+++ Assets/Project/Scripts/UI/Overlays/SkillTree/Tree/PassiveSkillTreeView.cs
@@ -2,15 +2,19 @@
 using System.Linq;
 using Itibsoft.PanelManager;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Scripts.SkillTree
 {
     public class PassiveSkillTreeView : PanelBase
     {
         public GameObject ParentObject => _parentObject;
+        public Button SkillListButton => _skillListButton;
         
         [SerializeField] private GameObject _parentObject;
         [SerializeField] private List<Skill> _skillsList = new();
+        
+        [SerializeField] private Button _skillListButton;
 
         public List<Skill> GetSkillsList()
         {
--- Assets/Project/Scripts/UI/Overlays/SkillTree/Tree/SkillTreeOverlayController.cs
+++ Assets/Project/Scripts/UI/Overlays/SkillTree/Tree/SkillTreeOverlayController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Itibsoft.PanelManager;
+using Project.Scripts.Overlays;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Project.Scripts.SkillTree
@@ -10,16 +12,30 @@ namespace Project.Scripts.SkillTree
     public class SkillTreeOverlayController : PanelControllerBase<PassiveSkillTreeView>
     {
         [Inject] private GlobalFactory _globalFactory;
+        [Inject] private IPanelManager _panelManager;
         
         private List<Skill> _skillsList = new();
         private GameObject _edgesParent;        
+        private Button _skillListButton;
+        
+        private SpellListOverlayController _spellListOverlayController;
         
         protected async override void Initialize()
         {
             _skillsList = Panel.GetSkillsList();
             _edgesParent = Panel.ParentObject;
-
+            _skillListButton = Panel.SkillListButton;
+            
+            _skillListButton.onClick.AddListener(OnSkillListButtonClick);
+            
             await CreateSkillsEdges(_skillsList);
+        }
+
+        private void OnSkillListButtonClick()
+        {
+            _spellListOverlayController = _panelManager.LoadPanel<SpellListOverlayController>();
+            _spellListOverlayController.Open();
+            Close();
         }
 
         private async UniTask CreateSkillsEdges(List<Skill> skillsList)
