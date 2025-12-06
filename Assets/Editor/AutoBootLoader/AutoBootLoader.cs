68e562fa7354f082fc7163bb1b5ad8ce1e9f8522
Leonid Tsypkin <caudate7855@gmail.com>
2025-12-06 22:24:22 +0700

    - import: DunGen samples imported

--- Assets/Editor/AutoBootLoader/AutoBootLoader.cs
+++ Assets/Editor/AutoBootLoader/AutoBootLoader.cs
@@ -8,22 +8,46 @@ namespace Editor.AutoBootLoader
     public static class AutoBootLoader
     {
         private const string BootScenePath = "Assets/Project/Scenes/Boot/BootScene.unity";
+        private const string PrefKey = "AutoBootLoader_Enabled";
 
         static AutoBootLoader()
         {
             EditorApplication.playModeStateChanged += OnPlayModeChanged;
         }
-        
+
         private static void OnPlayModeChanged(PlayModeStateChange state)
         {
+            if (!IsEnabled)
+                return;
+
             if (state == PlayModeStateChange.ExitingEditMode)
             {
-                if (SceneManager.GetActiveScene().path.Equals(BootScenePath)) 
+                if (SceneManager.GetActiveScene().path.Equals(BootScenePath))
                     return;
+
                 EditorApplication.isPlaying = false;
                 EditorSceneManager.OpenScene(BootScenePath);
                 EditorApplication.delayCall += () => { EditorApplication.isPlaying = true; };
             }
+        }
+
+        private static bool IsEnabled
+        {
+            get { return EditorPrefs.GetBool(PrefKey, true); }
+            set { EditorPrefs.SetBool(PrefKey, value); }
+        }
+
+        [MenuItem("Tools/Auto Boot Loader/Enabled")]
+        private static void Toggle()
+        {
+            IsEnabled = !IsEnabled;
+        }
+
+        [MenuItem("Tools/Auto Boot Loader/Enabled", validate = true)]
+        private static bool ToggleValidate()
+        {
+            Menu.SetChecked("Tools/Auto Boot Loader/Enabled", IsEnabled);
+            return true;
         }
     }
 }
