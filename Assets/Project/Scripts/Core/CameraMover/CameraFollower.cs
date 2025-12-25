a3636312acdf1a73fde55977465037a6b30a4091
Leonid Tsypkin <caudate7855@gmail.com>
2025-12-25 19:10:22 +0700

    - feat: wall fade logic created

--- Assets/Project/Scripts/Core/CameraMover/CameraFollower.cs
+++ Assets/Project/Scripts/Core/CameraMover/CameraFollower.cs
@@ -1,22 +1,76 @@
+using Project.Scripts;
+using Project.Scripts.Walls;
 using UnityEngine;
 using Zenject;
 
-namespace Project.Scripts
+public class CameraFollower : MonoBehaviour
 {
-    public class CameraFollower : MonoBehaviour
+    [Inject] private PlayerController _playerController;
+
+    [SerializeField]
+    private LayerMask _raycastMask;
+
+    [SerializeField]
+    private float _raycastInterval = 0.2f;
+
+    private readonly Vector3 _cameraOffsetPosition = new(-4, 10, -4);
+    private readonly Vector3 _cameraOffsetRotation = new(60, 45, 0);
+
+    private DungeonWall _currentWall;
+    private Camera _camera;
+
+    private void Start()
     {
-        [Inject] private PlayerController _playerController;
+        InvokeRepeating(nameof(RaycastToPlayer), 0f, _raycastInterval);
         
-        private readonly Vector3 _cameraOffsetPosition = new (-4, 10, -4);
-        private readonly Vector3 _cameraOffsetRotation = new (60, 45, 0);
+        _camera = GetComponent<Camera>();
+        _camera.backgroundColor = Color.black;
+    }
 
-        private void Update()
+    private void Update()
+    {
+        transform.position = _playerController.transform.position + _cameraOffsetPosition;
+
+        var rotation = transform.rotation;
+        rotation.eulerAngles = _cameraOffsetRotation;
+        transform.rotation = rotation;
+    }
+
+    private void RaycastToPlayer()
+    {
+        var origin = transform.position;
+        var target = _playerController.transform.position;
+        var direction = target - origin;
+        var distance = direction.magnitude;
+
+        if (Physics.Raycast(origin, direction, out var hit, distance, _raycastMask))
         {
-            transform.position = _playerController.transform.position + _cameraOffsetPosition;
+            Debug.Log(hit.collider.name);
+            
+            if (hit.collider.TryGetComponent<DungeonWall>(out var wall))
+            {
+                if (_currentWall != wall)
+                {
+                    ClearCurrentWall();
+                    _currentWall = wall;
+                    _currentWall.SetUpperPartTransparent(false);
+                }
 
-            var rotation = transform.rotation;
-            rotation.eulerAngles = _cameraOffsetRotation;
-            transform.rotation = rotation;
+                return;
+            }
         }
+
+        ClearCurrentWall();
+    }
+
+    private void ClearCurrentWall()
+    {
+        if (_currentWall == null)
+        {
+            return;
+        }
+
+        _currentWall.SetUpperPartTransparent(true);
+        _currentWall = null;
     }
 }
---
+++ Assets/Project/Scripts/Dungeon/Walls/DungeonWall.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Project.Scripts.Walls
+{
+    [Serializable]
+    public class DungeonWall : MonoBehaviour
+    {
+        [Header("Settings")] 
+        [SerializeField] private float _fadeDuration = 0.5f;
+        
+        [Space]
+        [SerializeField] private MeshRenderer _upperPartMeshRenderer;
+        
+        [Button]
+        public void SetUpperPartTransparent(bool condition)
+        {
+            var finalAlpha = condition ? 1f : 0f;
+            
+            _upperPartMeshRenderer.material.DOFade(finalAlpha, 0.5f);
+        }
+    }
+}
