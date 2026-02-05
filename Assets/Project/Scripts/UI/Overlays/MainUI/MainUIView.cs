7c4b7c3a54aa7f2e9ff98288a004732ba13ce0ae
Leonid Tsypkin <caudate7855@gmail.com>
2026-02-05 09:53:21 +0700

    - feat: unity version changed to 6.3

--- Assets/Project/Scripts/UI/Overlays/MainUI/MainUIView.cs
+++ Assets/Project/Scripts/UI/Overlays/MainUI/MainUIView.cs
@@ -1,15 +1,26 @@
 using System.Collections.Generic;
 using Itibsoft.PanelManager;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Project.Scripts
 {
     public class MainUIView : PanelBase
     {
+        [Title("Bars", horizontalLine: false, bold: true)]
         [field: SerializeField] public HpBar HpBar;
         [field: SerializeField] public EssenceBar EssenceBar;
         [field: SerializeField] public BossBar BossBar;
-        [field: SerializeField] public List<ElementalBarBase> ElementsSliders;
+        
+        [Space]
+        [Title("Resource bars", horizontalLine: false, bold: true)]
+        [field: SerializeField] public BonesBar BonesBar;
+        [field: SerializeField] public BloodBar BloodBar;
+        [field: SerializeField] public FleshBar FleshBar;
+        [field: SerializeField] public SoulBar SoulBar;
+        
+        [Space]
+        [Title("Spell buttons", horizontalLine: false, bold: true)]
         [field: SerializeField] public List<SpellUIButton> SpellUIButtons;
     }
 }
